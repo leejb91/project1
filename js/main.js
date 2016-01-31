f98da5c9f3843e5308fc873658c538589d9a27cb
Jerry Lee <leejb91@gmail.com>
2016-01-30 17:41:36 -0800

    add gameBegin

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 console.log("linked")
 
+var board = [pod1, pod2, pod3, pod4, pod5, pod6,
+            pod7, pod8, pod9, pod10, pod11, pod12];
+
+var gameBegin = function() {
+  for (var i = 0; i < board.length; i++) {
+    board[i].innerHTML = "4";
+  }
+}
+
+gameBegin();
+// var gameEnd = function() {
+//   if
+// }
